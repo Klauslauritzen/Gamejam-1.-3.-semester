e83321531063912c62d4e6cdcd904dd4724caed3
Mikkel Paulick <mikkelpaulick@hotmail.com>
2023-10-11 10:17:53 +0000

    Added attributes and properties to Player class

--- SurvivalGame/Player.cs
+++ SurvivalGame/Player.cs
@@ -2,5 +2,9 @@ namespace SurvivalGame;
 
 public class Player
 {
+    private Int32 daysPassed;
+    private Boolean isAlive;
+    public Int32 Hunger { get; set; }
+    public Int32 Stamina { get; set; }
     
 }
