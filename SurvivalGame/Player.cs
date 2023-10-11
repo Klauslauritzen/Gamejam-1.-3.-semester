7b1fcaf4350962ac5d43c158ff3527d9a8d424f6
Martin Peter Molbech <mpmo59239@edu.ucl.dk>
2023-10-11 13:43:52 +0200

    Implemented Player class. started work on basic game actions in program.cs

--- SurvivalGame/Player.cs
+++ SurvivalGame/Player.cs
@@ -2,5 +2,32 @@ namespace SurvivalGame;
 
 public class Player
 {
-    
+    public bool Alive { get; set; }
+    private int hunger {  get; set; }
+    private int stamina {  get; set; }
+    private int daysPassed { get; set; }
+
+    public Player ()
+    {
+        hunger = 100;
+        stamina = 100; 
+        daysPassed = 0;
+        Alive = true;
+    }
+
+    public void Eat()
+    {
+        stamina -= 5;
+        hunger += 5;
+    }
+    public void Sleep()
+    {
+        stamina += 5;
+        hunger -= 5;
+    }
+
+    public override string ToString()
+    {
+        return $"Your hunger is {hunger} \nYour stamina is {stamina} \nDays passed {daysPassed}";
+    }
 }
--- SurvivalGame/Program.cs
+++ SurvivalGame/Program.cs
@@ -4,7 +4,43 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to Island Survival!");
+            Player player = new Player();
+
+            do
+
+            {
+                Console.WriteLine(player);
+                Console.WriteLine("press 1 to eat");
+                Console.WriteLine("press 2 to sleep");
+                string input = "";
+                int playerInput;
+                while (!int.TryParse(input, out playerInput))
+                {
+                    input = Console.ReadLine();
+                }
+
+                switch (playerInput)
+                {
+                    case 1:
+                        player.Eat();
+                        break;
+                    case 2:
+                        player.Sleep();
+                        break;
+                    default:
+                        Console.WriteLine("idiot, try again!!");
+                        continue;
+
+                }
+                Console.WriteLine("Proceed to next action");
+                Console.ReadLine();
+                
+                Console.Clear();
+
+
+            }
+            while (player.Alive == true);
         }
     }
 }
