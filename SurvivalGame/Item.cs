e74435b2e3e79525d659220137ada29d5c7e5145
Martin Peter Molbech <mpmo59239@edu.ucl.dk>
2023-10-12 09:19:35 +0200

    added items, fixed player properties

---
+++ SurvivalGame/Item.cs
@@ -0,0 +1,24 @@
+﻿namespace SurvivalGame
+{
+    internal class Item
+    {
+        public ItemType Type {  get; set; }
+        public Quality Quality { get; set; }
+        public string Name { get; set; }
+
+        
+    }
+
+    public enum ItemType
+    {
+        Food,
+        Material,
+        Tool
+    }
+    public enum Quality
+    {
+        Low,
+        Medium,
+        High
+    }
+}
--- SurvivalGame/Player.cs
+++ SurvivalGame/Player.cs
@@ -2,30 +2,33 @@ namespace SurvivalGame;
 
 public class Player
 {
-    private int stamina {  get; set; }
+    private int energy {  get; set; }
     private int daysPassed { get; set; }
+    public bool Alive { get; set; }
+    private int time {  get; set; }
+    private int hunger { get; set; }
 
     public Player ()
     {
         hunger = 100;
-        stamina = 100; 
+        energy = 100; 
         daysPassed = 0;
         Alive = true;
     }
 
     public void Eat()
     {
-        stamina -= 5;
+        energy -= 5;
         hunger += 5;
     }
     public void Sleep()
     {
-        stamina += 5;
+        energy += 5;
         hunger -= 5;
     }
 
     public override string ToString()
     {
-        return $"Your hunger is {hunger} \nYour stamina is {stamina} \nDays passed {daysPassed}";
+        return $"Your hunger is {hunger} \nYour stamina is {energy} \nDays passed {daysPassed}";
     }
 }
